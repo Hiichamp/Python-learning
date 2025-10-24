f41874ecff18feb58211c95410aa4eb285181167
hiichamp <himanshukumar13333@gmail.com>
2025-10-24 09:14:27 +0530

    chapter-0

---
+++ index.py
@@ -0,0 +1,17 @@
+# print("hello world")
+
+# print(''' Hey [Name]—noticed your work on [specific team/project]. Quick idea: a focused 1:1 mentorship page aligned to your profile, LinkedIn‑friendly, and a “Book via WhatsApp” flow so conversations feel natural while you set your own rate. Open to a 60‑sec preview?​
+
+# If they’re very busy
+# Hi [Name]—one tap from your LinkedIn Featured to a concise 1:1 page → WhatsApp with a pre‑filled intro; you confirm time/rate in chat. Light, fast, trusted. Should I send a one‑pager? ''')
+
+
+# import pyttsx3
+# engine = pyttsx3.init()
+
+
+
+# engine.say("hii himanshu ,how are you")
+# engine.runAndWait()
+
+
