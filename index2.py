f78397f3f633723c8153acdb2127be93e328552f
hiichamp <himanshukumar13333@gmail.com>
2025-10-24 17:19:24 +0530

    chapter1

---
+++ index2.py
@@ -0,0 +1,42 @@
+# a = 2
+# b = 3
+
+# print("sum of two number is : ", a +b )
+
+
+# a = 6
+# b = 7
+
+
+# print( a * b)
+
+# a = int(input("first number is :"  ))
+# b = int(input("second number is :"  ))
+
+# print((a+b)/2)
+
+
+# a = int('2')
+# b = int('3')
+
+# print(a+b)
+
+
+# a = "3"
+
+# b = (a)
+
+# print(type(b))
+
+
+
+# a = int(input("first number is :"  ))
+# b = int(input("second number is :"  ))
+
+# print(a>b)
+
+
+# a=int(input())
+
+
+# print("sqaure of number is" , a*a)
