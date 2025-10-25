6de5ef6c54c85a554733f839ae66244f92021fa8
hiichamp <himanshukumar13333@gmail.com>
2025-10-25 20:32:37 +0530

    chapter2 -- ("strings")

---
+++ index3.py
@@ -0,0 +1,51 @@
+# strings
+
+
+# name = "Himansh"
+
+# print(name)
+
+
+# name = "himansh"
+# print(name.capitalize())
+
+
+# name = "HImansh"
+# print(name.lower())
+
+
+# name = "himansh"
+# print(name.__len__())
+
+# name = "himansh"
+# print(name[3])
+
+
+# name = "rocky"
+
+
+# print(name[1:3])
+
+# name ="himansh"
+
+# ty = name[-3:-1]
+
+
+# print(ty)
+
+
+# city = 'Agra'
+
+# log = city.replace("ra","riculture")
+
+# print(log)
+
+
+# city = input("enter your  name :")
+
+# print(f"Happy birthday  {city} ")
+
+
+# new = "here is something for you  \n\tDear hero,  \n\t\thow are you"
+
+# print(new)
