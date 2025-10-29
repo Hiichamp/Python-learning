69b19554e17c68a1d679f52015ea5e39db9a2c93
hiichamp <himanshukumar13333@gmail.com>
2025-10-29 09:22:50 +0530

    chapter3

---
+++ index4.py
@@ -0,0 +1,41 @@
+# list1 = ['grapes','banana','mango','litchi'] list is mutable means we add in also string for ex--
+
+
+
+# list1 = ['grapes','banana','mango','litchi']
+
+# list1.append("papaya")
+# print(list1)
+
+
+# list1 = ['grapes','banana','mango','litchi',12,15]
+
+# print(type(list1))   this is list of type not matter what in this list
+
+
+# list1 = [1222,12,1,3,132]
+
+# list1.append(23)
+# list1.sort()
+# print(list1)
+
+# list1 = [12,12,32,"ramu",'rohan']
+
+# list1.insert(1,"cock")  insert means add in index number to rteplace other things in this things any -- int,str
+
+# print(list1)
+
+
+# list1 = ['grapes','banana','mango','litchi']
+
+# print(list1[1:3])
+
+# list1 = ('grapes','banana','mango','litchi')
+
+# print(type(list1)) this is tuple and in this tuple we can't change anythings
+
+# list1 = ('grapes','banana','mango','litchi')
+
+
+
+# print(list1)
