336d054b409e555b5f9c5ad26bd960695c4b1db9
hiichamp <himanshukumar13333@gmail.com>
2025-10-30 22:26:44 +0530

    chapter-4 (dictionary)

---
+++ index5.py
@@ -0,0 +1,56 @@
+# dictionary
+# s ={     
+#     "rahul":100,
+#     "mehak":200,
+#     'rohan':130
+# }
+
+# print(type(s))
+
+# marks = {
+#     "rahul" :100,
+#     'parel':120,
+#     'aniket':120
+# }
+
+
+# print(marks.keys())
+
+
+# marks = {
+#     "rahul" :100,
+#     'parel':120,
+#     'aniket':120
+# }
+
+
+# print(marks.values())
+
+
+
+# s1={1,23,43,45}
+
+# s2={1,233,4453,45}
+
+
+# print(s1.union(s2))
+
+
+# s1={1,23,43,45}
+
+# s2={1,233,4453,45}
+
+
+# print(s1.intersection(s2))
+
+
+
+# s1 ={1,2,3,233,45}
+
+# s2 ={2,4,54,233}
+
+# print(s1-s2)
+
+
+
+
