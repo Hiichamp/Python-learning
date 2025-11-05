75aeba6b52f3bfd9cbdc8a06208643711ad58577
hiichamp <himanshukumar13333@gmail.com>
2025-11-05 09:26:54 +0530

    chapter -6

---
+++ index7.py
@@ -0,0 +1,45 @@
+# loops
+
+
+# for i in range(1,10):
+#     print(i)
+   
+
+
+# n = int(input("emter any number"))
+
+# for i in range(1,10):
+#     print(f"{n } * {i} = {n * i}")
+   
+
+
+# l =["himanshu" , "hritik",'hitesh',"mohan","rohar"]
+
+
+# for name in l:
+#     if(name.startswith("h")):
+#         print(f"Hello {name}")
+
+
+# n = int(input("emter any number"))
+
+# i = 1
+
+# while(i<11):
+#     print(f"{n } * {i} = {n * i}")
+#     i+=1
+
+
+# n = int(input("emter any number  "))
+
+# for i in range(2,n):
+#     if(n%i == 0):
+#         print("Number is not Prime")
+#         break
+# else:
+#     print("Number is prime")
+
+n = int(input("number : "))
+
+for i in range(1,11):
+    print(f"{n} * {11-i} = {n * (11-i)}")
