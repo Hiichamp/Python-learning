2120be3b15804c4ff56a5b217c9b052fc6392e1d
hiichamp <himanshukumar13333@gmail.com>
2025-11-06 08:53:40 +0530

    chapter-7

---
+++ index8.py
@@ -0,0 +1,42 @@
+# function ----   def
+
+# def greatestNumb(a,b,c):
+#     if(a>b and a>c):
+#         return a
+#     elif(b>a and b>c):
+#         return b
+#     elif(c>a and c>b):
+#         return c
+    
+    
+# a =1
+# b=51
+# c =9
+# print(greatestNumb(a,b,c))
+
+
+# def f_to_c(f):
+#     return 5*(f-32)/9
+
+
+# f = int(input("enter your temp in  F : "))
+# c = f_to_c(f)
+# print(f"{round(c,2)} °C")
+    
+
+# def pattern(n):
+#     if(n==0):
+#         return 
+#     print("*" * n)
+#     pattern(n-1)
+
+# pattern(6)
+
+
+
+# def mt_to_cm(mt):
+#     return mt * 100
+
+# a = float(input("enter your mt : "))
+
+# print(f"{mt_to_cm(a)} cm")
