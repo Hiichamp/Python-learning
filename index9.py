ab0fa043c1e79b62030363f47168f310c0bace9e
hiichamp <himanshukumar13333@gmail.com>
2025-11-09 09:01:49 +0530

    chapter - 8

---
+++ index9.py
@@ -0,0 +1,61 @@
+# files read and write
+
+# f = open("poem.txt")
+
+# read = f.read()
+# print(read)
+# f.close()
+
+
+# f = open("poem.txt")
+
+# read = f.read()
+
+# if("twinkle" in read):
+#     print("twinkle is present")
+
+# else:
+#     print("not present")
+# f.close()
+
+
+
+# def genTable(n):
+#     table = ""
+#     for i in range(1,11):
+#         table += f"{n} * {i} = {n * i}\n"
+
+#     with open("table/tables_{n}.txt","w") as f:
+#         f.write(table)
+
+
+# for i in range(2,21):
+#     genTable(i)
+
+
+
+
+# word = "donkey"
+
+# with open("poem.txt","r") as f:
+#   content =  f.read()
+
+# contentNew = content.replace(word,"#####")
+
+# with open("poem.txt","w") as f:
+#   content =  f.write(contentNew)
+    
+
+
+# with open("poem.txt") as f:
+#     content = f.read()
+
+
+# with open("poem3.txt",'w') as f:
+#     f.write(content)
+
+
+# with open("poem3.txt",'w') as f:
+#     f.write("")
+
+
