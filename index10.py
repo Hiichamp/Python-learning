5aca03079ab66eb949b04c1920a9a340e754bab8
hiichamp <himanshukumar13333@gmail.com>
2025-11-11 08:04:29 +0530

    chapter-9

---
+++ index10.py
@@ -0,0 +1,34 @@
+# # object oriented programing
+
+# class student:
+#     classes =  "10"
+#     def __init__(self,name,roll,school):
+#         self.name = name,
+#         self.roll = roll
+#         self.school = school
+
+
+# print('abh', 10, 'sldjc')
+
+# p =student("rahul", 12, 'sldjic')
+
+# print(p.name,p.roll,p.school)
+
+
+# class calculator:
+#     def __init__(self,n):
+#         self.n = n
+#     def sqaure(self):
+#         print(f"the sqaure of {self.n} is = {self.n*self.n}")
+    
+#     def cube(self):
+#         print(f"the cube of {self.n} is = {self.n*self.n*self.n}")
+
+#     def sqRoot(self):
+#         print(f"the sqRoot of {self.n} is = {int(self.n**1/2)}")
+
+
+# a = calculator(4)
+# a.sqaure()
+# a.cube()
+# a.sqRoot()
