5a0fcbf858b98674ffcf2e5cd47c9744e75fb754
hiichamp <himanshukumar13333@gmail.com>
2025-11-16 10:36:13 +0530

    chapter - 11

---
+++ index12.py
@@ -0,0 +1,39 @@
+# try:
+#     with open("poem.txt","r") as f:
+#         print(f.read())
+# except Exception as e:
+#     print(e)
+
+
+# try:
+#     with open("article.txt","r") as f:
+#         print(f.read())
+# except Exception as e:
+#     print(e)
+# try:
+#     with open("article.txt","r") as f:
+#         print(f.read())
+# except Exception as e:
+#     print(e)
+
+
+
+# l = [1,2,5,6,9]
+
+# for i, item in enumerate(l):
+#     if(item ==22 or item ==52):
+#         print(item)
+
+
+# print("no any number find in database")
+
+
+# n =4
+
+
+# table = [n*i for i in range(1,11)]
+
+
+# print( table)
+
+
