4655a6e2204a7cf4e601ec39863cf3283943b717
hiichamp <himanshukumar13333@gmail.com>
2025-11-18 09:14:58 +0530

    chapter-12

---
+++ AdvancePy/form.py
@@ -0,0 +1,45 @@
+from functools import reduce
+
+# name = input("Enter your name : ")
+# age = int(input("enter your age: "))
+# phoneNo = int(input("enter your phone no"))
+
+
+# Format = "you name is {} and your age is {} and your PhoneNo is {}".format(name,age,phoneNo)
+
+# print(Format)
+
+
+
+# table = [str(7*i) for i in range(1,11)]
+
+# s = "\n".join(table)
+
+# print(s)
+
+
+# def divisible(n):
+#     if(n%5 == 0):
+#         return True
+#     return False
+
+
+# li = [1221,35454,56545,101030]
+
+# s = list(filter(divisible,li))
+
+
+# print(s)
+
+# def greatest(a,b):
+#     if(a>b):
+#         return a
+#     return b
+
+
+# li = [1221,35454,5546656545,101030]
+
+
+# s = reduce(greatest,li)
+
+# print(s)
